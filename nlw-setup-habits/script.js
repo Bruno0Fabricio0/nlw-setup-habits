367415d9127320959f1af9d7d34b6112e9580f62
bruno0fabricio0 <bruno0.fabricio0@gmail.com>
2023-01-19 20:54:38 -0300

    adicionando funções

--- nlw-setup-habits/script.js
+++ nlw-setup-habits/script.js
@@ -1,13 +1,38 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = { 
-    trabalhar: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07', '01-08', '01-09', '01-10',
-    '01-11', '01-12', '01-13', '01-14', '01-15', '01-16', '01-17', '01-18', '01-19', '01-20',
-    '01-21', '01-22', '01-23', '01-24', '01-25', '01-26', '01-27', '01-28', '01-29', '01-30', '01-31'], 
+button.addEventListener('click', add) 
+form.addEventListener("change", save)
 
+function add () {
+  const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
 
   }
   
-  nlwSetup.setData(data)
-  nlwSetup.load()
+    alert("Adicionado com Sucesso")
+
+  nlwSetup.addDay(today)
+}
+
+
+
+
+function save () {
+  localStorage.setItem("computertech@habits", JSON.stringify (nlwSetup.data))
+
+}
+//const data = { 
+ //   trabalhar: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07', '01-08', '01-09', '01-10',
+ //   '01-11', '01-12', '01-13', '01-14', '01-15', '01-16', '01-17', '01-18', '01-19', '01-20',
+ //   '01-21', '01-22', '01-23', '01-24', '01-25', '01-26', '01-27', '01-28', '01-29', '01-30', '01-31'], }
+  
+    const data = JSON.parse(localStorage.getItem("computertech@habits")) || {}
+     nlwSetup.setData(data)
+     nlwSetup.load()
